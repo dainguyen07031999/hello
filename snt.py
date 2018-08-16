d797a80ed0e6966199b50081cff83b25837d3e95
tudiena30070 <tudien1609@gmail.com>
2018-08-16 13:45:38 +0700

    upload files

---
+++ snt.py
@@ -0,0 +1,18 @@
+import stdio
+import math
+stdio.write('Nhap mot so: ')
+n = stdio.readInt()
+count = 0
+i = 2
+while count < n:
+	flag = True
+	c = int(math.sqrt(i))
+	for k in range(2, c+1):
+		if i % k == 0:
+			flag = False
+			break 
+	if flag:
+		count = count + 1
+	if (count < n):
+		i = i + 1
+stdio.writeln('So nguyen to thu ' + str(n) + ' la: ' + str(i))
